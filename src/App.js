249f1e82b88820f666f31f0366a2ca826ccfca3d
kGrand254 <kevinkimutai62@gmail.com>
2022-05-30 11:18:18 +0300

    fixed bug during production build

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { React, useEffect } from "react";
 
 import { Route, Routes } from "react-router-dom";
 
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 import "./App.css";
 import { Navbar } from "./components";
